9aa190ab905359f7e7e238b7a1161531a850814d
kienbv114 <144187555+kienbv114@users.noreply.github.com>
2023-09-06 21:28:50 +0700

    Add files via upload

---
+++ Sort/src/view/main.java
@@ -0,0 +1,21 @@
+
+package view;
+
+import common.Library;
+import Controller.program;
+import Model.element;
+/**
+ *
+ * @author hieu9
+ */
+public class main {
+    public static void main(String[] args) {
+        Library library = new Library();
+        element element = new element();
+        element.setSize(library.getInt("Enter number of array", 1, 100));
+        element.setArray(library.CreateArray(element.size));
+        new program(element).run();
+
+    }
+    
+}
